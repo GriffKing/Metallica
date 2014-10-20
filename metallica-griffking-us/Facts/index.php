bd6688b7c488fb2f0abd0647f6a3a03611344f30
Griff <swag@griffking.us>
2014-10-20 19:43:34 -0400

    Metallica Music Project
    I finally finished it. View here: http://metallica.griffking.us/

---
+++ metallica-griffking-us/Facts/index.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en/us">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Metallica - GriffKing.us</title>
+    
+    <link rel="stylesheet" href="main.css">
+    
+    <!-- Bootstrap -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style>
+        body { padding-bottom: 60px; }
+    </style>
+  </head>
+  <body>
+     <!-- NAVIGATION -->    
+     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="http://griffking.us/projects">Metallica</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li><a href="http://metallica.griffking.us/">Home <span class="glyphicon glyphicon-home"></span></a></li>
+            <li class="active"><a href="http://metallica.griffking.us/facts">Facts <span class="glyphicon glyphicon-list"></span></a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="http://metallica.griffking.us/sources">Sources <span class="glyphicon glyphicon glyphicon-ok-circle"></span></a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- nav container -->
+    </nav>
+    
+    <center>
+    <!-- JUMBOTRON (HEADING) -->
+    <div class="jumbotron">
+        <h1>Metallica</h1>
+        <img src="http://goo.gl/1y5wOr" width="50%" height="50%" style="border: 5px solid #1a1a1a">  
+        <br><br><p>A project by Griff King and Trevor Gardler.</p>
+        <p><a class="btn btn-primary btn-lg" role="button" href="http://metallica.griffking.us/">About</a></p>
+    </div>
+    </center>
+    <!-- BODY (GRID) -->
+    <center>
+    <div class="col-md-12">
+        <div class="container">
+            <div class="row span9 text-center">
+                <h3>Just a few of Metallica's many albums.<br></h3>
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                        </div>
+                        <div class="panel-body">
+                            <div class="col-xs-6 col-md-3">
+                                <a target="_blank" href="http://goo.gl/gpj2cl" class="thumbnail">
+                                    <img width="100%" height="100%" src="http://goo.gl/pID9H2" alt="The Black Album">
+                                </a>
+                            </div>
+                            <div class="col-xs-6 col-md-3">
+                                <a target="_blank" href="http://goo.gl/PD4Z1Q" class="thumbnail">
+                                    <img width="100%" height="100%" src="http://goo.gl/8cRlJu" alt="Master of Puppets">
+                                </a>
+                            </div>
+                            <div class="col-xs-6 col-md-3">
+                                <a target="_blank" href="http://goo.gl/e2mSLP" class="thumbnail">
+                                    <img width="100%" height="100%" src="http://goo.gl/flhosz" alt="Ride the Lightning">
+                                </a>
+                            </div>
+                            <div class="col-xs-6 col-md-3">
+                                <a target="_blank" href="http://goo.gl/CZYZVb" class="thumbnail">
+                                    <img width="100%" height="100%" src="http://goo.gl/PFh2kt" alt="...And Justice For All">
+                                </a>
+                            </div>
+                            <br><br><br><br><br><br><br><br><br><br><br><br>
+                            
+                            <hr>
+                            <h3>Facts:</h3>
+                            <ul class="list-group">
+                                <li class="list-group-item">The current members of Metallica are James Hetfield, Lars Ulrich, Kirk Hammett, and Robert Trujillo.
+</li>
+                                <li class="list-group-item">James Hetfield and Lars Ulrich started Metallica.</li>
+                                <li class="list-group-item">Metallica’s longest song is 9 minutes and 57 seconds long, and is entitled “Suicide & Redemption.”</li>
+                                <li class="list-group-item">Metallica’s shortest song is 3 minutes and 8 seconds long, and is entitled “Motorbreath.”</li>
+                                <li class="list-group-item">Dave Mustaine was Metallica’s first lead guitarist.</li>
+                                <li class="list-group-item">In total, Metallica has had 8 members that have either came and gone, or are still in the band.</li>
+                                <li class="list-group-item">Metallica’s Black Album has sold 15,620,000 units.</li>
+                                <li class="list-group-item">Metallica have had 5 of their records hit the number one spot on the billboard 100 charts.</li>
+                                <li class="list-group-item">Metallica have made 10 full length albums.</li>
+                                <li class="list-group-item">Metallica have played 1,724 concerts in the last 30 years.</li>
+                                <li class="list-group-item">That means they play around 57.5 concerts per year.</li>
+                                <li class="list-group-item">Metallica have played in San Francisco more than any other city.</li>
+                                <li class="list-group-item">Metallica have played in 60 different countries.</li>
+                                <li class="list-group-item">Metallica have played their song “Master of Puppets” live 1,256 times.</li>
+                                <li class="list-group-item">Metallica’s old bass player Cliff Burton was killed on September 27th, 1986 when Metallica’s tour bus flipped over in Sweden while they were on tour.</li>
+                                <li class="list-group-item">Metallica have won 14 Grammy awards.</li>
+                                <li class="list-group-item">Metallica’s guitarist, Kirk Hammett, is a vegetarian.</li>
+                                <li class="list-group-item">Metallica’s drummer, Lars Ulrich, collects a lot of art.</li>
+                                <li class="list-group-item">James Hetfield learned to play piano before the guitar.</li>
+                                <li class="list-group-item">Robert Trujillo has a side project outside of Metallica called Mass Mental.</li>
+                                <li class="list-group-item">It is said that Metallica spent $400,000 on their music video for their song “The Memory Remains.”</li>
+                                <li class="list-group-item">The first band Cliff Burton made EZ-Street.</li>
+                                <li class="list-group-item">Lars Ulrich and James Hetfield voiced on the disney show “Dave The Barbarian,” for one episode.</li>
+                                <li class="list-group-item">In 1984, Metallica’s “Ride The Lightning” was pressed on a misprinted cover, with the glowing outline being green instead of the first intended blue. There are only 400 copies of the green outlined misprint in existence.</li>
+                                <li class="list-group-item">Darren Aronofsky, an award-winning actor made the music video for the Metallica song “The View.”</li>
+                                <li class="list-group-item">The first band James Hetfield was in was called “Obsession.”</li>
+                                <li class="list-group-item">Metallica cancelled a show in Evansville, Indiana when James Hetfield broke his wrist while skateboarding.</li>
+                                <li class="list-group-item">For 2 months, Metal Church’s John Marshall took over for Hetfield when he had his broken wrist.</li>
+                                <li class="list-group-item">Lars Ulrich played drums for the Mercyful Fate song “Return of the Vampire.”</li>
+                                <li class="list-group-item">Lars Ulrich sold a painting from his collection of paintings for $13,500,000.</li>
+                                <li class="list-group-item">Cliff Burton came up with the album name “Kill ‘Em All.”</li>
+                                <li class="list-group-item">Dave Mustaine was kicked out of Metallica a month before the recording session of the album “Kill ‘Em All.”</li>
+                                <li class="list-group-item">Kirk Hammett had to re-work Dave’s guitar solos on the album “Kill ‘Em All.”</li>
+                                <li class="list-group-item">Some songs were songs various Metallica members had made with their previous bands.</li>
+                                <li class="list-group-item">“Seek And Destroy,” the song by Metallica, was once used as a wrestling entrance song.</li>
+                                <li class="list-group-item">Metallica has sold over 3,000,000 copies of their record “Kill ‘Em All.”</li>
+                                <li class="list-group-item">Metallica has ended their setlist at concerts with their song “Seek and Destroy” almost every time they’ve played since 2003.</li>
+                                <li class="list-group-item">There are shoes with the album art for “Kill ‘Em All” on it.</li>
+                                <li class="list-group-item">In March 1986, Metallica’s album “Master of Puppets” reached number 41 on the UK most popular album charts.</li>
+                                <li class="list-group-item">Metallica’s album “Kill ‘Em All” was released in the summer of 1983.</li
+                            </ul>    
+                        </div>
+                    </div>
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3 class="panel-title"> Musical Example <span class="glyphicon glyphicon-music"></span></h3>
+                        </div>
+                        <div class="panel-body">
+                            <div class="embed-responsive embed-responsive-4by3">
+                                <iframe width="420" height="315" src="//www.youtube.com/embed/CD-E-LDc384" frameborder="0" allowfullscreen></iframe>
+                            </div>
+                                <div class="panel panel-default">
+                                    <div class="panel-body">   
+                                        <hr>
+                                        <p>
+                                        This is the music video for Metallica's well known song "Enter Sandman".<hr>
+                                            <small>
+                                                <h5>Credit:</h5> Enter Sandman [Official Music Video]
+From the album "Metallica"
+
+Director: Wayne Isham
+Filmed in June 1991 in Los Angeles, CA
+Video Premiere Date: July 30, 1991
+
+© 1991 Metallica
+                                            </small>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>  
+                </div>
+                </center>
+            </div>
+        <!--</div>
+    </div>-->
+    </center>
+    <nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
+        <div class="container">
+            <a class="navbar-brand" href="#">Metallica Project</a>
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Other Links <span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="http://metallica.griffking.us/facts">Facts</a></li> 
+                        <li><a href="http://metallica.griffking.us/sources">Sources</a></li>
+                        <li><a target="_blank" href="http://metallica.com">Official Metallica Site</a></li>
+                        <li><a target="_blank" href="http://github.com/griffking/metallica/">Source Code</a></li> 
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </nav>   
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  </body>
+</html>
